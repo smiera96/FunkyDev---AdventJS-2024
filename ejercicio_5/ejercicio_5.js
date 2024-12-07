5bbb92a4054c6b289cf23afc09fbff85f1c4c38d
Sergio Miera <s.miera96@gmail.com>
2024-12-07 17:11:48 +0100

    Ejercicio 5

---
+++ ejercicio_5/ejercicio_5.js
@@ -0,0 +1,26 @@
+/**
+ * @param {{ type: 'I' | 'R', size: number }[]} shoes
+ * @returns {number[]} Available shoes
+ */
+function organizeShoes(shoes) {
+    let groupShoes = []
+
+    shoes.forEach(item => {
+        groupShoes[item.size] ??= {I: 0, R: 0}
+        groupShoes[item.size][item.type]++
+    })
+
+    return groupShoes.flatMap((item, size) => {
+        const min = Math.min(item.R, item.I)
+        return Array(min).fill(size)
+    });
+}
+
+const shoes = [
+    { type: 'I', size: 38 },
+    { type: 'R', size: 38 },
+    { type: 'R', size: 42 },
+    { type: 'I', size: 41 },
+    { type: 'I', size: 42 }
+]
+console.log(organizeShoes(shoes))
