7794d44b796844bdac7db88c10d9569b405805a5
Sergio Miera <s.miera96@gmail.com>
2024-12-07 17:43:19 +0100

    Ejercicio 1

--- ejercicio_1/ejercicio_1.js
+++ ejercicio_1/ejercicio_1.js
@@ -1,5 +1,5 @@
 function prepareGifts(gifts) {
-    return [...new Set(gifts)].sort((a, b) => a - b);
+    return [...new Set(gifts)].sort((a, b) => a - b)
 }
 
 const gifts1 = [3, 1, 2, 3, 4, 2, 5]
