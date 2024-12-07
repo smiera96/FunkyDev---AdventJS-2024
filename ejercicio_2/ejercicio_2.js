69ef657b6c0b3dc67145834fef156763511d5fff
Sergio Miera <s.miera96@gmail.com>
2024-12-07 17:35:05 +0100

    Ejercicio 2

---
+++ ejercicio_2/ejercicio_2.js
@@ -0,0 +1,10 @@
+function createFrame(names) {
+    const maxL = names.reduce((max, text) => Math.max(max, text.length), 0);
+    const border = '*'.repeat(maxL + 4)
+    const framedNames = names
+        .map(name => `* ${name.padEnd(maxL, ' ')} *`)
+        .join('\n');
+    return `${border}\n${framedNames}\n${border}`
+}
+
+console.log(createFrame(['midu', 'madeval', 'educalvolpz']))
